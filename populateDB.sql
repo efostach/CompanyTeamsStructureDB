207b6b9290149ed371a9e094dcfab7dc48c0cec6
Helen Fostach <elena.fostach@gmail.com>
2019-11-16 11:46:12 +0300

    Fixed typos in tables name

--- populateDB.sql
+++ populateDB.sql
@@ -8,8 +8,8 @@ INSERT INTO `customers` VALUES (1,'NEC'),(2,'Vodafone'),(3,'Telefonica');
 
 INSERT INTO `employees` VALUES (1,'Ivan','Ivanov',3,0),(2,'Anton','Antonov',2,0),(3,'Victor','Victorov',1,0),(4,'Semen','Semenov',3,0),(5,'Petr','Petrov',1,0);
 
-INSERT INTO `customer_projectset` VALUES (2,1),(2,2),(1,3),(2,3),(3,3),(1,4),(2,4),(3,4),(1,5),(2,5),(2,6),(3,6);
+INSERT INTO `customer_projects` VALUES (2,1),(2,2),(1,3),(2,3),(3,3),(1,4),(2,4),(3,4),(1,5),(2,5),(2,6),(3,6);
 
-INSERT INTO `employee_skillset` VALUES (2,1),(3,1),(4,1),(5,1),(1,2),(2,2),(5,3),(4,4),(5,4),(1,5),(3,5),(5,5),(3,6),(4,6),(2,7),(4,7);
+INSERT INTO `employee_skills` VALUES (2,1),(3,1),(4,1),(5,1),(1,2),(2,2),(5,3),(4,4),(5,4),(1,5),(3,5),(5,5),(3,6),(4,6),(2,7),(4,7);
 
-INSERT INTO `project_teamset` VALUES (1,1),(2,1),(5,1),(6,1),(3,2),(4,2),(1,3),(2,3),(4,3);
+INSERT INTO `project_teams` VALUES (1,1),(2,1),(5,1),(6,1),(3,2),(4,2),(1,3),(2,3),(4,3);
